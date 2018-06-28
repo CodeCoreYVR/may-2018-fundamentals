4cda0da037cf6450ad5de7fd3dc601cc7dacf02c
Jacob Tran <jacobtran@gmail.com>
2018-06-27 20:02:48 -0700

    add day 10

---
+++ day10/review.js
@@ -0,0 +1,11 @@
+// good evening fundies!
+// 1. jquery
+// 2. firebase
+// 3. data structures: databases
+
+// todo list
+// chat apps
+// code snippets
+
+// web site vs. web application?
+// informative vs interactive
